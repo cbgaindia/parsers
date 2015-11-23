0d8e2ee7b497ac5d7fb4bb8ec7b5c79c91d64051
gaurav <gggodhwani@gmail.com>
2015-11-24 05:17:54 +0530

    Keyword extractor for Union Budgets

---
+++ keywords_extractor.py
@@ -0,0 +1,75 @@
+'Class for extracting keywords for PDF Documents in a directory'
+
+import csv
+import glob,os
+import logging
+from logging.config import fileConfig
+from lxml import etree
+import re
+
+DOC_DIR = "union_budgets/2015-16/Expenditure Budget/Volume II/"
+OUT_FILE = "union_budgets/2015-16/expenditure_budget_keywords_map.csv"
+OUT_CSV_HEADER = ["Department", "Keywords"]
+TEMP_INDEX_FILE = "/tmp/page.html"
+TEMP_HTML_FILE = "/tmp/pages.html"
+LOG_FILE = "/tmp/log"
+SKIP_WORDS = ["total", "b. investment in public enterprises", "c. plan outlay", "other programmes", "grand total", "central plan", "state plan", "union territory plans", "union territory plans (with legislature)"]
+
+fileConfig('parsers/logging_config.ini')
+logger = logging.getLogger()
+
+class KeywordsExtractor(object):
+    def extract_bold_keywords(self):
+        '''Extract Bold keywords from all PDF documents in the directory and generate a CSV mapping
+        '''
+        self.skip_words = SKIP_WORDS
+        self.department_name = ""
+        with open(OUT_FILE, "wb") as csv_file:
+            csv_writer = csv.writer(csv_file, delimiter=',') 
+            csv_writer.writerow(OUT_CSV_HEADER)
+            for file_name in glob.glob("%s*.pdf" % DOC_DIR):
+                try:
+                    self.department_name = os.path.basename(file_name).lower().split(".pdf")[0].decode('utf-8')
+                    html_obj = self.get_html_object(file_name)
+                    bold_text_phrases = self.get_bold_text_phrases(html_obj)
+                    csv_writer.writerow([os.path.basename(file_name).split(".pdf")[0].decode('utf-8'), str(bold_text_phrases)]) 
+                    logger.info("Processing PDF document for department: %s" % self.department_name)
+                except Exception, e:
+                    logger.error("Unable to extract keywords for department: %s, error_message: %s" % (department_name, error_message))
+
+    def get_bold_text_phrases(self, html_obj): 
+        '''Extract bold text phrases from input HTML object 
+        '''
+        dom_tree = etree.HTML(html_obj.read())
+        bold_text_phrases = []
+        for phrase in dom_tree.xpath("//b/text()"):
+            phrase = self.clean_extracted_phrase(phrase)
+            if phrase in self.skip_words or re.search(r'^no. [0-9]+/|^no. [0-9]+|^total-|^total -', phrase) or phrase == self.department_name.encode('utf-8'):
+                continue
+            if re.search(r'[a-z]{2,}', phrase):
+                if not phrase in bold_text_phrases and len(phrase.split(" ")) > 1:
+                    bold_text_phrases.append(phrase.strip())
+        return bold_text_phrases
+
+    def clean_extracted_phrase(self, phrase):
+        '''Cleanse phrase text to remove unwanted characters and words
+        '''
+        phrase = phrase.lower()
+        phrase = phrase.encode('utf-8').replace('\xa0', ' ').replace('\xc2', '').strip()
+        phrase = re.sub(r'[^a-zA-Z\d\)]$', '', phrase)
+        phrase = re.sub(r', ETC.$|, etc.$', '', phrase)
+        phrase = re.sub(r'\s{2,}', ' ', phrase)
+        phrase = re.sub(r'^other ', '', phrase).strip()
+        return phrase
+
+    def get_html_object(self, file_name):
+        '''Convert PDF file into HTML file using pdftohtml(http://sourceforge.net/projects/pdftohtml/)
+        '''
+        command = "pdftohtml '%s' '%s' > %s" % (file_name, TEMP_INDEX_FILE, LOG_FILE)
+        os.system(command)
+        html_obj = open(TEMP_HTML_FILE, "rb")
+        return html_obj
+
+if __name__ == '__main__':
+    obj = KeywordsExtractor()
+    obj.extract_bold_keywords()
